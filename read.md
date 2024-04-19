94ffdc6580d7243c9675f87d2e823f716e8cec74
Tyne Crow <t.crow@auckland.ac.nz>
2024-04-20 08:57:07 +1200

    initial commit

---
+++ read.md
@@ -0,0 +1,7 @@
+# WDCC - server-side JavaScript, Node.js, Express & APIs
+
+## Links:
+
+* Link to slides: [tinyurl.com/wdcc-js-24](http://tinyurl.com/wdcc-js-24)
+* You will need to install Node.js: [nodejs.org](http://nodejs.org/)
+* Suggested IDE: [code.visualstudio.com](http://code.visualstudio.com/)
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+// Set's our port to the PORT environment variable, or 3000 by default if the env is not configured.
+const PORT = process.env.PORT ?? 3000;
+
+// TODO Create the Express server
+
+
+// TODO Configure middleware with app.use() (CORS support, JSON parsing support, static files support)
+
+
+// TODO Your application routes here
+
+
+// TODO Start the server
+
